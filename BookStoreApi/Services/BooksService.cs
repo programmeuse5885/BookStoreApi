7e781d8295262c7d17a3e1d6495536127cb05408
Carolina <Carolina@DESKTOP>
2023-12-08 12:08:17 -0600

    Add project files.

---
+++ BookStoreApi/Services/BooksService.cs
@@ -0,0 +1,38 @@
+﻿using BookStoreApi.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace BookStoreApi.Services
+{
+    public class BooksService
+    {
+        private readonly IMongoCollection<Book> _booksCollection;
+        public BooksService(IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(bookStoreDatabaseSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(bookStoreDatabaseSettings.Value.DatabaseName);
+            _booksCollection = mongoDatabase.GetCollection<Book>(bookStoreDatabaseSettings.Value.BooksColletionName);
+
+        }
+
+        //Busca todos
+        public async Task<List<Book>> GetAsync() =>
+            await _booksCollection.Find(_ => true).ToListAsync();
+
+        //Busca por id
+        public async Task<Book?> GetAsync(string id) =>
+            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        //Inserta
+        public async Task CreateAsync(Book newBook) =>
+            await _booksCollection.InsertOneAsync(newBook);
+
+        //Actualiza
+        public async Task UpdateAsync(string id, Book updateBook) =>
+            await _booksCollection.ReplaceOneAsync(x=>x.Id==id, updateBook);
+
+        //Elimina
+        public async Task RemoveAsync(string id) =>
+            await _booksCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
